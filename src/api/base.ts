f90b3364725a40938125564a3d4451b09b264097
seriparkdev <qkrtpfl9228@gmail.com>
2022-11-08 20:26:10 +0900

    fix: 401(unauthorized)와 관련된 에러 처리
    오류로 인해서 데이터를 받아오지 않는 상황이 발생했을 때 다시 로그인 페이지로 돌아가도록 구현

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,22 +1,33 @@
 import axios from 'axios';
-import { Console } from 'console';
+import { ROUTE } from '../constants/routes';
 import { UserToken } from '../utils/auth';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = UserToken.get();
-  config.headers = {
-    Authorization: `Bearer ${token}`,
-  };
-  config.paramsSerializer = {
-    serialize: (params) => {
-      return new URLSearchParams(params).toString();
-    },
-  };
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = UserToken.get();
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    config.paramsSerializer = {
+      serialize: (params) => {
+        return new URLSearchParams(params).toString();
+      },
+    };
+    return config;
+  },
+  (error) => {
+    const errorStatus = error.response.status;
+
+    if (errorStatus === 401) {
+      UserToken.remove();
+      window.location.replace(ROUTE.LOGIN);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
