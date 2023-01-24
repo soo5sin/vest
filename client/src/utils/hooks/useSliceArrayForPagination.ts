5d566eedf011158eae50a6b4178a1564fac03fe6
seriparkdev <qkrtpfl9228@gmail.com>
2023-01-24 20:31:23 +0900

    file: pagination과 관련된 파일들 삭제
    무한 스크롤로 로직 변경

--- client/src/utils/hooks/useSliceArrayForPagination.ts
+++
@@ -1,6 +0,0 @@
-export const sliceArrayForPagenation = (array: any[], Currentpage: number, limit: number) => {
-  const beginIndex = limit * (Currentpage - 1);
-  const endIndex = beginIndex + limit;
-  const slicedArray = array.slice(beginIndex, endIndex);
-  return slicedArray;
-};
