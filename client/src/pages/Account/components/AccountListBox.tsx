1e8964ccb0d1ee936ef71cde7e910d0a65fdb236
seriparkdev <qkrtpfl9228@gmail.com>
2023-01-30 14:57:30 +0900

    refactor: account 페이지의 뷰와 로직 분리 (#5)

--- client/src/pages/Account/components/AccountListBox.tsx
+++ client/src/pages/Account/components/AccountListBox.tsx
@@ -1,40 +1,12 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Error from '../../../components/shared/error/Error';
 import Thead from '../../../components/shared/table/Thead';
-import { useAppDispatch, useAppSelector } from '../../../store';
-import { getAccountsThunk } from '../../../store/reducers/accounts';
 import { Account } from '../../../types/account';
 import TbodyRow from './TbodyRow';
-import { getUsersThunk } from '../../../store/reducers/users';
-import { useInView } from 'react-intersection-observer';
+import useAccountList from '../hooks/useAccountList';
 
 export default function AccountListBox() {
-  const dispatch = useAppDispatch();
-  const accounts = useAppSelector((state) => state.reducers.accounts);
-  const users = useAppSelector((state) => state.reducers.users);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-  const [ref, inView] = useInView();
-
-  const getAccounts = async () => {
-    const response = await dispatch(getAccountsThunk({ _page: page, _limit: 20 }));
-    const newAccounts = response.payload;
-    setHasMore(newAccounts.length === 20);
-  };
-
-  useEffect(() => {
-    if (!inView || !hasMore || accounts.isLoading || users.isLoading) return;
-    setPage((prev) => prev + 1);
-  }, [inView, hasMore]);
-
-  useEffect(() => {
-    getAccounts();
-  }, [page]);
-
-  useEffect(() => {
-    dispatch(getUsersThunk());
-  }, []);
+  const { ref, accounts, users } = useAccountList();
 
   if (accounts.error || users.error) return <Error error="data fetching error" />;
 
---
+++ client/src/pages/Account/hooks/useAccountList.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import useInfiniteScroll from '../../../hooks/useInfiniteScroll';
+import { useAppDispatch, useAppSelector } from '../../../store';
+import { getAccountsThunk } from '../../../store/reducers/accounts';
+import { getUsersThunk } from '../../../store/reducers/users';
+
+export default function useAccountList() {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state) => state.reducers.users);
+  const accounts = useAppSelector((state) => state.reducers.accounts);
+  const { page, setPage, hasMore, setHasMore, ref, inView } = useInfiniteScroll();
+
+  const getAccounts = async () => {
+    const response = await dispatch(getAccountsThunk({ _page: page, _limit: 20 }));
+    setHasMore(response.payload.length === 20);
+  };
+
+  useEffect(() => {
+    if (!inView || !hasMore || accounts.isLoading || users.isLoading) return;
+    setPage(page + 1);
+  }, [inView, hasMore]);
+
+  useEffect(() => {
+    getAccounts();
+  }, [page]);
+
+  useEffect(() => {
+    dispatch(getUsersThunk());
+  }, []);
+
+  return { ref, accounts, users, inView };
+}
